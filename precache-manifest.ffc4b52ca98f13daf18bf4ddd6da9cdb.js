41808c45524307ee46967425ac51acc87c0d85f9
milek agrawal <milekag01@gmail.com>
2020-04-17 21:38:53 +0530

    Updates

---
+++ precache-manifest.ffc4b52ca98f13daf18bf4ddd6da9cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9123ef8f9668e71d96aac36f5ea0107b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c71ac0a36f783e3c224",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b870e0eaa11101f97cbd",
+    "url": "/static/js/2.51c2cd0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51c2cd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c71ac0a36f783e3c224",
+    "url": "/static/js/main.b1a659ba.chunk.js"
+  },
+  {
+    "revision": "9aefc1278f3ef2a0bf75",
+    "url": "/static/js/runtime-main.026c7b51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
